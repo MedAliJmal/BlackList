00fd36fd59d92d4cad9dd5479db8cd4416abc000
MedAliJmal <speth4tunisia@gmail.com>
2023-03-01 22:08:30 +0100

    test

--- src/components/headerNav/HeaderNav.js
+++ src/components/headerNav/HeaderNav.js
@@ -6,57 +6,55 @@ const HeaderNav = () => {
   return (
     <div className="nave">
       <Navbar bg="dark" variant="dark">
-        <Container className="counta">
-          <Navbar.Brand>
-            <img
-              src="https://cdn.discordapp.com/attachments/874040015779426315/1078973018522537994/blacklist.png"
-              alt="blacKLIST"
-              id="blacklist"
-            />
-          </Navbar.Brand>
-          <Nav className="me-auto navee">
-            <Nav.Link>
+        <Navbar.Brand>
+          <img
+            src="https://cdn.discordapp.com/attachments/874040015779426315/1078973018522537994/blacklist.png"
+            alt="blacKLIST"
+            id="blacklist"
+          />
+        </Navbar.Brand>
+        <Nav className="me-auto navee">
+          <Nav.Link>
+            {" "}
+            <Link
+              className="headtitle"
+              to={"/"}
+              style={{ textDecoration: "none" }}
+            >
+              Members
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              to={"/Category"}
+              className="headtitle"
+              style={{ textDecoration: "none" }}
+            >
+              Race Category
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            {" "}
+            <Link to={"/RaceRules"} className="headtitle">
               {" "}
-              <Link
-                className="headtitle"
-                to={"/"}
-                style={{ textDecoration: "none" }}
-              >
-                Members
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link
-                to={"/Category"}
-                className="headtitle"
-                style={{ textDecoration: "none" }}
-              >
-                Race Category
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
+              Race Rules
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            {" "}
+            <Link to={"/Category/Drift"} className="headtitle">
               {" "}
-              <Link to={"/RaceRules"} className="headtitle">
-                {" "}
-                Race Rules
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
+              Drift
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            {" "}
+            <Link to={"/Category/Drag"} className="headtitle">
               {" "}
-              <Link to={"/Category/Drift"} className="headtitle">
-                {" "}
-                Drift
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              {" "}
-              <Link to={"/Category/Drag"} className="headtitle">
-                {" "}
-                Drag
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Container>
+              Drag
+            </Link>
+          </Nav.Link>
+        </Nav>
       </Navbar>
     </div>
   );
